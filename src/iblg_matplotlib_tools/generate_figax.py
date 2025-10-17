d99c12e5c0662c5df841599bbd5253efd2a94fba
Ian Billinge <ian.billinge@gmail.com>
2025-10-16 20:48:04 -0400

    feat: added figsize option to generate_figax

--- src/iblg_matplotlib_tools/generate_figax.py
+++ src/iblg_matplotlib_tools/generate_figax.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
 
-def generate_figax():
-    fig = plt.figure()
+def generate_figax(figsize: tuple[float] = (5.0, 4.0)):
+    """Generates a figure, axis, and gridspec.
+
+    figsize: tuple[float]
+    The figure size. Defaults to (5.,4.).
+    """
+    fig = plt.figure(figsize=figsize)
     gs = GridSpec(1, 1, left=0.25, right=0.97, top=0.97, bottom=0.15)
     ax = fig.add_subplot(gs[0, 0])
     return fig, ax, gs
